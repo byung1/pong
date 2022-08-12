436b9733568395b3108e527685b656746217cb94
Bill Yung <97060613+byung1@users.noreply.github.com>
2022-08-12 15:32:32 -0400

    Added Push Library (#2)

--- main.lua
+++ main.lua
@@ -1,11 +1,26 @@
+--[[
+    Downloaded from https://github.com/Ulydev/push
+
+    Push is a library that allows us to render a game with lower resolution (virtual dimensions)
+    within an arbitrary screen size (window dimensions)
+]]
+push = require 'push'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
 
 --[[
     Constructor
 ]]
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    
+    -- Initializes our virtual resolution within our window no matter what the
+    -- dimenstions are and replaces love.window.setMode
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
@@ -13,14 +28,30 @@ function love.load()
 end
 
 --[[
+    Keyboard handling
+]]
+function love.keypressed(key)
+    -- terminates the application
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+--[[
     This function is called after every update
 ]]
 function love.draw()
+    -- begin rendering at virtual resolution
+    push:apply('start')
+
     love.graphics.printf(
         'Hello Pong!', 
         0, 
-        WINDOW_HEIGHT / 2 - 6, 
-        WINDOW_WIDTH, 
+        VIRTUAL_HEIGHT / 2 - 6, 
+        VIRTUAL_WIDTH, 
         'center'
     )
+
+    -- end rendering at virtual resolution
+    push:apply('end')
 end
