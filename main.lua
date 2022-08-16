8d18fcb7e7033f29d0f1eb1f6089aa2cf54f759b
Bill Yung <bill.ken.yung@gmail.com>
2022-08-16 19:06:26 -0400

    added ball logic

--- main.lua
+++ main.lua
@@ -53,6 +53,47 @@ end
     Runs every frame, with `dt` passed in as our delta in seconds
 ]]
 function love.update(dt)
+    -- detect collision for player1
+    if ball:collides(player1) then
+        ball.dx = -ball.dx * 1.03
+        ball.x = player1.x + player1.width + 1
+
+        if ball.dy < 0 then
+            ball.dy = -math.random(10, 150)
+        else
+            ball.dy = math.random(10, 150)
+        end
+    end
+
+    -- detect collision for player2
+    if ball:collides(player2) then
+        ball.dx = -ball.dx * 1.03
+        ball.x = player2.x - player2.width - 1
+
+        if ball.dy < 0 then
+            ball.dy = -math.random(10, 150)
+        else
+            ball.dy = math.random(10, 150)
+        end
+    end
+
+    -- detect upper and lower screen boundary collision
+    if ball.y <= 0 then
+        ball.y = 0
+        ball.dy = -ball.dy
+    end
+
+    if ball.y >= VIRTUAL_HEIGHT - ball.height then
+        ball.y = VIRTUAL_HEIGHT - ball.height
+        ball.dy = -ball.dy
+    end
+
+    -- detect whether a ball has cross the sides of the screen
+    if ball.x <= 0 or ball.x >= VIRTUAL_WIDTH then
+        gameState = 'ready'
+        ball:reset()
+    end
+
     -- player 1 movement
     if love.keyboard.isDown('w') then
         player1.dy = -PADDLE_SPEED
@@ -63,7 +104,7 @@ function love.update(dt)
     end
 
      -- player 2 movement
-     if love.keyboard.isDown('up') then
+    if love.keyboard.isDown('up') then
         player2.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('down') then
         player2.dy = PADDLE_SPEED
